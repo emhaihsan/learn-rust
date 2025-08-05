3dc52e33520d96647f159941487249ffe42f297f
Muhammad Ihsan <emhihsan@gmail.com>
2025-08-05 10:55:05 +0700

    Remove target/ directory from Git history and update .gitignore

--- chapter_4_ownership/target/debug/chapter_4_ownership.d
+++
@@ -1,1 +0,0 @@
-/Users/emhaihsan/Desktop/rustbook/chapter_4_ownership/target/debug/chapter_4_ownership: /Users/emhaihsan/Desktop/rustbook/chapter_4_ownership/src/main.rs
--- chapter_4_ownership/target/debug/deps/chapter_4_ownership-01640146f5bbb9dc.d
+++
@@ -1,5 +0,0 @@
-/Users/emhaihsan/Desktop/rustbook/chapter_4_ownership/target/debug/deps/chapter_4_ownership-01640146f5bbb9dc.d: src/main.rs
-
-/Users/emhaihsan/Desktop/rustbook/chapter_4_ownership/target/debug/deps/chapter_4_ownership-01640146f5bbb9dc: src/main.rs
-
-src/main.rs:
